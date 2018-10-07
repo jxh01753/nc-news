37f4218d134220310113fe4bd495a439f2187e02
John Harrington <32112213+jxh01753@users.noreply.github.com>
2018-10-07 18:58:04 +0100

    Sets up blank profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default Profile;
